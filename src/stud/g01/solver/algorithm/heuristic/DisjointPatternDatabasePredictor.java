398d1b7a6faedb0dfb2c1ca17e531b795233f940
naxx <1426384849@qq.com>
2025-10-31 12:57:50 +0800

    加上disjointPatternDatabase

---
+++ src/stud/g01/solver/algorithm/heuristic/DisjointPatternDatabasePredictor.java
@@ -0,0 +1,11 @@
+package stud.g01.solver.algorithm.heuristic;
+
+import core.problem.State;
+import core.solver.algorithm.heuristic.Predictor;
+
+public class DisjointPatternDatabasePredictor implements Predictor {
+    @Override
+    public int heuristics(State state, State goal) {
+        return 0;
+    }
+}
